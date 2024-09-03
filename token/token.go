6e58039f105188bc771d34d92d5a445be3d63bec
Michalis Papamichael <michalis.papamichael@outlook.com>
2024-09-03 21:01:51 +0300

    finished 1.3

---
+++ token/token.go
@@ -0,0 +1,46 @@
+package token
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + Literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "-"
+
+	// Delimiters
+	COMMA     = "."
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookipIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
