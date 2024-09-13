5fd188c76c4e447469dac656f17530b8a3c9182c
Michalis Papamichael <michalis.papamichael@outlook.com>
2024-09-13 20:04:39 +0300

    finished 2.8.6 - removing todos

--- parser/parser.go
+++ parser/parser.go
@@ -139,9 +139,11 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 		return nil
 	}
 
-	// TODO: We're skipping the expressions until we
-	// encounter a semicolon
-	for !p.curTokenIs(token.SEMICOLON) {
+	p.nextToken()
+
+	stmt.Value = p.parseExpression(LOWEST)
+
+	for p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
 
@@ -153,9 +155,9 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 
 	p.nextToken()
 
-	//TODO: We are skipping the expressions until
-	// we encounter semicolon
-	if !p.curTokenIs(token.SEMICOLON) {
+	stmt.ReturnValue = p.parseExpression(LOWEST)
+
+	if p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
 
