b05f272e3c97319e7cae9bf7f753e6483303fae4
Michalis Papamichael <michalis.papamichael@outlook.com>
2024-09-20 19:42:53 +0300

    finished 4.2.3 - evaluating strings

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -33,6 +33,8 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		env.Set(node.Name.Value, val)
 	case *ast.Identifier:
 		return evalIdentifier(node, env)
+	case *ast.StringLiteral:
+		return &object.String{Value: node.Value}
 	// Expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
